3b69115bc8f9672aa8318c88e5c3d99aa658099c
ZFlem96 <zackzacharyfleming@gmail.com>
2017-05-15 16:29:46 -0600

    created enemy class

---
+++ TextAdventure/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Enemy
+    {
+        //Enemies will be randomly picked and will be given an random value
+        /*Values
+         * names
+         * type
+         * 
+         *
+         */ 
+    }
+}
