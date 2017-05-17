496f86e2014beae19f949712042f57420b463951
Nightangel17 <loganwakely66@yahoo.com>
2017-05-17 16:13:53 -0600

    Armor updates

--- TextAdventure/Armor.cs
+++ TextAdventure/Armor.cs
@@ -8,20 +8,53 @@ namespace TextAdventure
 {
     class Armor : Item
     {
+        MainWindow mw = new MainWindow();
         //durabilty
         //defense
-        enum ArmorTypes
-        {
-            Leatherarmor,
-            Bronzearmor,
-            Ironarmor,
-            Steelarmor
-        };
+    
 
         public void setArmor()
         {
+            string leatherarmor = "Leather Armor";
+            string bronzearmor = "Bronze Armor";
+            string ironarmor = "Iron Armor";
+            string steelarmor = "Steel Armor";
 
+            if (tbxAction.contains( leatherarmor)) {
+                armor.durability = 3;
+                player.defense = 6;
+                    }
+            else if (tbx.Actions.contains( bronzearmor))
+            {
+                armor.durability = 5;
+                player.defense = 8;
+            }
+            else if (tbx.Actions.contains(ironarmor))
+            {
+                armor.durability = 7;
+                player.defense = 10;
+            }
+            else if (tbx.Actions.contains(steelarmor))
+            {
+                armor.durability = 9;
+                player.defense = 12;
+            }
+            else
+            {
+                armor.durability = 0;
+                player.defense = 1;
+            }
         }
+
+        /*
+         *armorThing(String action){
+         * 
+         * if tbx.Action.contains(string leatherarmor){
+         * armor.durability = 3
+         * armor.defense = 6
+         * 
+         * 
+         */
 
  
 
