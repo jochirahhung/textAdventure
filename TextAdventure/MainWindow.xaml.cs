88ea1e7bd49de036ce26f723716aa6bd32801ffe
Joshua <jyoung@student.neumont.edu>
2017-05-31 16:43:12 -0600

    Fighting, new pictures, and enemy logic

--- TextAdventure/MainWindow.xaml.cs
+++ TextAdventure/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace TextAdventure
 
         private void game()
         {
+            Random rnd = new Random();
             Inventory i = new Inventory();
             Enemy e = new Enemy();
             BitmapImage title = new BitmapImage(new Uri("/TextAdventure;component/Images/Title.png", UriKind.Relative));
@@ -46,20 +47,26 @@ namespace TextAdventure
             BitmapImage Ogre = new BitmapImage(new Uri("/TextAdventure;component/Images/Ogre.png", UriKind.Relative));
             BitmapImage Orc = new BitmapImage(new Uri("/TextAdventure;component/Images/Orc.png", UriKind.Relative));
             BitmapImage Thug = new BitmapImage(new Uri("/TextAdventure;component/Images/thug.png", UriKind.Relative));
+
+            BitmapImage Key = new BitmapImage(new Uri("/TextAdventure;component/Images/Key.png", UriKind.Relative));
+            BitmapImage Leather = new BitmapImage(new Uri("/TextAdventure;component/Images/leatherarmor.png", UriKind.Relative));
             string action = tbxAction.Text;
             if ((action.Equals("start") || action.Equals("Start")))
             {
                 imgLevel.Source = lvl1;
                 tbxlStory.Text = "You are in the bedroom, you see a chest in the room, it appears to be unlocked, the door on the other hand is not.";
-                imgenemy.Source = dragon;
             }
-            else if (action.Equals("Open chest"))
+            else if (action.Equals("Open chest", StringComparison.CurrentCultureIgnoreCase))
             {
+                tbxlStory.Text = "You open the chest and inside is the leather armor, and a key";
                 imgLevel.Source = chest;
+                imgArmor.Source = Leather;
+                imgKey.Source = Key;
             }
             else if (action.Equals("Leave chest"))
             {
                 imgLevel.Source = lvl1;
+                tbxlStory.Text = "You are in the bedroom, you see a chest in the room, it appears to be unlocked, the door on the other hand is not.";
             }
 
             if (action.Contains("Armor"))
@@ -70,18 +77,22 @@ namespace TextAdventure
             if (tbxArmor.Text.Contains("Leather Armor"))
             {
                 tbxDefense.Text = "6";
+                imgArmor.Source = null;
             }
             else if (tbxArmor.Text.Contains("Bronze Armor"))
             {
                 tbxDefense.Text = "8";
+                imgArmor.Source = null;
             }
             else if (tbxArmor.Text.Contains("Iron Armor"))
             {
                 tbxDefense.Text = "10";
+                imgArmor.Source = null;
             }
             else if (tbxArmor.Text.Contains("Steel Armor"))
             {
                 tbxDefense.Text = "12";
+                imgArmor.Source = null;
             }
 
             if (action.Contains("Sword"))
@@ -92,23 +103,28 @@ namespace TextAdventure
             if (tbxWeapon.Text.Contains("Bronze Sword"))
             {
                 tbxDamage.Text = "6";
+                imgSword.Source = null;
             }
             else if (tbxWeapon.Text.Contains("Iron Sword"))
             {
                 tbxDamage.Text = "8";
+                imgSword.Source = null;
             }
             else if (tbxWeapon.Text.Contains("Steel Sword"))
             {
                 tbxDamage.Text = "10";
+                imgSword.Source = null;
             }
             else if (tbxWeapon.Text.Contains("Titanium Sword"))
             {
                 tbxDamage.Text = "12";
+                imgSword.Source = null;
             }
 
             if (action.Contains("Key"))
             {
                 tbxKey.Text = i.generateInventory(action);
+                imgKey.Source = null;
             }
 
             if (action.Equals("Open Door", StringComparison.CurrentCultureIgnoreCase))
@@ -126,18 +142,69 @@ namespace TextAdventure
                     tbxEnemyLVL.Text = e.getLevel().ToString();
                 }
 
-                if (action.Contains("goblin") || action.Contains("Goblin"))
+                else if (action.Contains("goblin") || action.Contains("Goblin"))
                 {
                     e.setEnemy(EnemeyTypes.ORC, int.Parse(tbxLevel.Text));
                     tbxEnemyHeatlh.Text = e.getHealth().ToString();
                     tbxEnemyLVL.Text = e.getLevel().ToString();
                 }
-                if (action.Contains("orc") || action.Contains("Orc"))
+                else if (action.Contains("orc") || action.Contains("Orc"))
                 {
                     e.setEnemy(EnemeyTypes.ORC, int.Parse(tbxLevel.Text));
                     tbxEnemyHeatlh.Text = e.getHealth().ToString();
                     tbxEnemyLVL.Text = e.getLevel().ToString();
                 }
+                else if (action.Contains("ogre") || action.Contains("Ogre"))
+                {
+                    e.setEnemy(EnemeyTypes.OGRE, int.Parse(tbxLevel.Text));
+                    tbxEnemyHeatlh.Text = e.getHealth().ToString();
+                    tbxEnemyLVL.Text = e.getLevel().ToString();
+                }
+                else if (action.Contains("thug") || action.Contains("Thug"))
+                {
+                    e.setEnemy(EnemeyTypes.THUG, int.Parse(tbxLevel.Text));
+                    tbxEnemyHeatlh.Text = e.getHealth().ToString();
+                    tbxEnemyLVL.Text = e.getLevel().ToString();
+                }
+            }
+
+            if (action.Equals("Attack", StringComparison.CurrentCultureIgnoreCase))
+            {
+                int healthLoss = int.Parse(tbxEnemyHeatlh.Text);
+                healthLoss = healthLoss - int.Parse(tbxDamage.Text);
+                if (healthLoss <= 0)
+                {
+                    imgenemy.Source = null;
+                    int exp = int.Parse(tbxEXP.Text);
+                    exp = exp + e.getEXP();
+                    tbxEXP.Text = exp.ToString();
+                    tbxEnemyHeatlh.Text = "0";
+                    tbxEnemyLVL.Text = "0";
+                }
+                else
+                {
+                    tbxEnemyHeatlh.Text = healthLoss.ToString();
+                    int chance = rnd.Next(1, 4);
+                    if (chance >= 3)
+                    {
+                        int health = int.Parse(tbxHealth.Text);
+                        health = health - e.getDamage();
+                        tbxHealth.Text = health.ToString();
+                    }
+                }
+            }
+            int trueHealth = int.Parse(tbxHealth.Text);
+            if (trueHealth <= 0)
+            {
+                tbxlStory.Text = "GAME OVER!!!!!";
+            }
+            int lvlUP = int.Parse(tbxEXP.Text);
+            if (lvlUP >= 25)
+            {
+                int lvl = int.Parse(tbxLevel.Text);
+                lvl = lvl+1;
+                tbxLevel.Text = lvl.ToString();
+                tbxEXP.Text = "0";
             }
 
             tbxAction.Text = "";
