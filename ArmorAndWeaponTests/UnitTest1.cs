0274ad3e4a8366d6f0a11f5c1449e353899f2b66
Joshua <jyoung@student.neumont.edu>
2017-06-01 14:41:12 -0600

    Made Them Pass

---
+++ ArmorAndWeaponTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextAdventure;
+
+namespace ArmorAndWeaponTests
+{
+    [TestClass]
+    public class ArmorAndWeapons
+    {
+        [TestMethod]
+        public void generateAWeapon()
+        {
+            string weapon = "bronze sword";
+            Inventory i = new Inventory();
+            Assert.IsNotNull(i.generateInventory(weapon));
+            
+        }
+
+        [TestMethod]
+        public void generateArmor()
+        {
+            string weapon = "steel armor";
+            Inventory i = new Inventory();
+            Assert.IsNotNull(i.generateInventory(weapon));
+        }
+        [TestMethod]
+        public void generateAWeaponWrong()
+        {
+            string weapon = "Plastic Sword";
+            Inventory i = new Inventory();
+            Assert.IsNull(i.generateInventory(weapon));
+
+        }
+
+        [TestMethod]
+        public void generateArmorWrong()
+        {
+            string weapon = "Plastic Armor";
+            Inventory i = new Inventory();
+            Assert.IsNull(i.generateInventory(weapon));
+        }
+    }
+}
